eb74020e1932dc0d0b8379747297ad1c665d2c9f
udodinho <udodinho200@gmail.com>
2024-11-14 10:06:11 +0100

    ft: implement delete single product

--- contracts/src/InventoryManagement.sol
+++ contracts/src/InventoryManagement.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.24;
 
 contract InventoryManagement {
     error ProductDoesNotExist();
+    error AddressZeroDetected();
 
     struct Products {
         uint256 productID;
@@ -11,7 +12,7 @@ contract InventoryManagement {
         uint256 quantity;
         address uploader;
         uint256 dateAdded;
-        uint256 reorderPoint;
+        string barcode;
     }
 
     mapping(uint256 => Products) private products;
@@ -29,15 +30,19 @@ contract InventoryManagement {
     
     event ProductUpdated(uint256 indexed productID, string indexed productName, uint256 productPrice, uint256 quantity);
     event ProductDeleted(uint256 indexed productID);
-    event StockLow(uint256 indexed productID, string indexed productName, uint256 quantity);
+    // event StockLow(uint256 indexed productID, string indexed productName, uint256 quantity);
 
     function addNewProduct(
         string memory _productName,
         uint256 _productPrice,
         uint256 _quantity,
-        uint256 _reorderPoint
+        string _barcode
     ) public {
+        if(msg.sender == address(0)) revert AddressZeroDetected();
+
         uint256 productID = nextProductID++;
+
+        string memory barcode = bytes(_barcode).length > 0 ? _barcode : "";
 
         products[productID] = Products({
             productID: productID,
@@ -46,14 +51,12 @@ contract InventoryManagement {
             quantity: _quantity,
             uploader: msg.sender,
             dateAdded: block.timestamp,
-            reorderPoint: _reorderPoint
+            barcode: barcode
         });
 
         productIDs.push(productID);
 
         emit ProductAdded(productID, _productName, _productPrice, _quantity, msg.sender, block.timestamp);
-
-        _checkStockLevels(productID);
     }
 
     function updateProduct(
@@ -61,30 +64,54 @@ contract InventoryManagement {
         string memory _productName,
         uint256 _productPrice,
         uint256 _quantity,
-        uint256 _reorderPoint
+        string _barcode
     ) public {
-        if (products[productID].productID == 0) revert ProductDoesNotExist();
+        if(msg.sender == address(0)) revert AddressZeroDetected();
+
+        if(products[productID].productID == 0) revert ProductDoesNotExist();
+
+        string memory barcode = bytes(_barcode).length > 0 ? _barcode : "";
 
         products[productID].productName = _productName;
         products[productID].productPrice = _productPrice;
         products[productID].quantity = _quantity;
-        products[productID].reorderPoint = _reorderPoint;
+        products[productID].barcode = barcode;
 
         emit ProductUpdated(productID, _productName, _productPrice, _quantity);
-
-        _checkStockLevels(productID);
     }
 
     function getProduct(uint256 productID) public view returns (Products memory) {
-        if (products[productID].productID == 0) revert ProductDoesNotExist();
+        if(msg.sender == address(0)) revert AddressZeroDetected();
+
+       if(products[productID].productID == 0) revert ProductDoesNotExist();
         return products[productID];
     }
 
     function getAllProduct() public view returns (Products[] memory) {
+        if(msg.sender == address(0)) revert AddressZeroDetected();
+
         Products[] memory allProducts = new Products[](productIDs.length);
         for (uint256 i = 0; i < productIDs.length; i++) {
             allProducts[i] = products[productIDs[i]];
         }
         return allProducts;
     }
+
+    function deleteProduct(uint256 productID) public {
+        if(msg.sender == address(0)) revert AddressZeroDetected();
+
+        if (products[productID].productID == 0) revert ProductDoesNotExist();
+
+        delete products[productID];
+
+        emit ProductDeleted(productID);
+    }
+
+    // Internal function to check stock levels and emit alert if below reorder point
+    // function _checkStockLevels(uint256 productID) internal {
+    //     Products storage product = products[productID];
+    //     if (product.quantity <= product.reorderPoint) {
+    //         emit StockLow(productID, product.productName, product.quantity);
+    //     }
+    // }
 }
