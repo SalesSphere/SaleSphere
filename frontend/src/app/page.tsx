fe9c91d1eb8d5eb2ba0065ae99ad8a10c1b92d8c
Superior <samsonaderonmu2021@gmail.com>
2024-11-16 01:07:20 +0100

    feat: integrate Thirdweb client and add ConnectButton for wallet connections

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,55 @@
-import SimpleNav from "@/components/simple-nav";
+"use client";
 
-const page = () => {
+import SimpleNav from "@/components/simple-nav";
+import { ConnectButton } from "thirdweb/react";
+import { client } from "./client";
+import { inAppWallet } from "thirdweb/wallets";
+import { defineChain } from "thirdweb";
+
+
+const wallets = [
+  inAppWallet({
+    auth: {
+      options: [
+        "google",
+        "discord",
+        "telegram",
+        "farcaster",
+        "email",
+        "x",
+        "passkey",
+        "phone",
+      ],
+    },
+  }),
+];
+  const page = () => {
   return (
     <div>
       <SimpleNav />
-      SaleSphere{" "}
+      <ConnectButton
+        client={client}
+        wallets={wallets}
+        connectModal={{ size: "compact" }}
+      />
+
+      {/* <div className="flex justify-center mb-20">
+        <ConnectButton
+          client={client}
+          connectModal={{ size: "compact" }}
+          wallets={[
+            inAppWallet({
+              auth: {
+                options: ["google", "email", "phone", "wallet"],
+              },
+            }),
+          ]}
+          accountAbstraction={{
+            chain: defineChain(4202),
+            sponsorGas: true,
+          }}
+        />
+      </div> */}
     </div>
   );
 };
