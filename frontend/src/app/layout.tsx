43d568f6f5b8758f4524db12ebbfd627b153cd47
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-11-16 20:58:54 +0100

    feat:wagmi connection

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -22,7 +22,9 @@ export default async function RootLayout({
   <ThirdwebProvider>
       <html lang="en">
         <body className={inter.className}>
-          <main className="container mx-auto p-4 md:p-6">{children}</main>
+          <main className="container mx-auto p-4 md:p-6">
+            <Provider2 cookies={cookies}>{children}</Provider2>
+          </main>
         </body>
       </html>
     </ThirdwebProvider>
